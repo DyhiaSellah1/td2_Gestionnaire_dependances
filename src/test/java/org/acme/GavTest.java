c5f13e6b9903f6590da0d425863005936bc9c6aa
dihia <sellahdyhia@gmail.com>
2026-09-24 12:20:04 +0200

    td2-q04-red

---
+++ src/test/java/org/acme/GavTest.java
@@ -0,0 +1,15 @@
+package org.acme;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GavTest {
+
+    @Test
+    void shouldParseGroup() {
+        Gav gav = Gav.parse("org.acme:lib-a:1.0.0");
+
+        assertEquals("org.acme", gav.getGroup());
+    }
+}
